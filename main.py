377d87adb44210c64ffb8debcf3621266e7ced8e
Peter Janočko <peter.janocko@kosickaakademia.sk>
2023-11-25 14:57:30 +0100

    added pdf to text

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+import PyPDF2
 
 app = FastAPI()
 
+def pdf_to_text(pdf_path):
+    text = ""
+    with open(pdf_path, "rb") as file:
+        pdf_reader = PyPDF2.PdfFileReader(file)
+        num_pages = pdf_reader.numPages
+
+        for page_number in range(num_pages):
+            page = pdf_reader.getPage(page_number)
+            text += page.extractText()
+
+    return text
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Hello": "World"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/upload-pdf")
+async def upload_pdf(file_path: str):
+    text_content = pdf_to_text(file_path)
+    return {"text_content": text_content}
